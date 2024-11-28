33857d3df27462375260d3f2223842eea99bdb17
Fred <xpz11@hotmail.fr>
2024-11-28 18:02:20 +0100

    ajout des ressources pour bosser sur les tuto gsap

---
+++ src/js/script_perso.js
@@ -0,0 +1,8 @@
+// Génère moi la structure d'un fichier jquery
+
+$(document).ready(function() {
+    // Code à exécuter une fois que le DOM est chargé
+    console.log('Le DOM est chargé !');
+
+    gsap.to('.container_exo_gsap h1', {x: 500});
+});
